b916cbbc787f080e765f2210f8bef8f191196b64
Latif <latif_1995@live.no>
2025-12-29 00:39:19 +0100

    middleware added, and some express middleware in app.js

--- app.js
+++ app.js
@@ -6,14 +6,24 @@ import authRouter from "./routes/auth.routes.js";
 import userRouter from "./routes/user.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import conncectToDatabase from "./database/mongodb.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
+
+// expres middleware
+app.use(express.json()); // allow to parse json request body
+app.use(express.urlencoded({ extended: false })); // helps to prosess form data
+app.use(cookieParser()); // remember to import it - reads cookies and makes them available in req.cookies
 
 console.log("App is starting...");
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
+
+// middleware for handling errors
+app.use(errorMiddleware);
 
 app.get("/", (req, res) => {
   res.send("Velkommen til subscription service !");
