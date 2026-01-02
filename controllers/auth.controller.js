d004a807d3cb4890b2f5480c2538ef1ea2eafcf2
Latif <latif_1995@live.no>
2026-01-03 00:08:34 +0100

    tokens bcrypt

---
+++ controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const singUp = async (req, res, next) => {
+  // Implementation sign-up logic
+};
+
+export const signIn = async (req, res, next) => {
+  // Implementation sign-in logic
+};
+
+export const signOut = async (req, res, next) => {
+  // Implementation sign-out logic
+};
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,14 +1,9 @@
 import { Router } from "express";
+import { signIn, singUp, signOut } from "../controllers/auth.controller";
 const authRouter = Router();
 
-authRouter.post("/sign-up", (req, res) => {
-  res.send({ title: "User sign-up endpoint" });
-});
-authRouter.post("/sign-in", (req, res) => {
-  res.send({ title: "User sign-in endpoint" });
-});
-authRouter.post("/sign-out", (req, res) => {
-  res.send({ title: "User sign-out endpoint" });
-});
+authRouter.post("/sign-up", singUp);
+authRouter.post("/sign-in", signIn);
+authRouter.post("/sign-out", signOut);
 
 export default authRouter;
